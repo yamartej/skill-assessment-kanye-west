0afa5a31d0ec264792263bf827f5c60ca23893e8
Jairo Yamarte <jairoyamarte@gmail.com>
2024-01-07 09:37:19 -0400

    Update

---
+++ resources/js/components/EjemploComponente.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Hola desde Vue.js</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('Componente montado.');
+    }
+}
+</script>
